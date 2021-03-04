d04cb625318edda6fb6cdd35461a6f5ef4b5f64b
partsdecisions <g.anikevics@student.utwente.nl>
2021-03-04 02:57:30 +0100

    initializing

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react"
+import './App.css'
+
+// require('dotenv').config(); ????
+
+function App() {
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("This part will execute everytime the page is re-renderd");
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleReq);  
+    const data = await response.json;
+    console.log(data);
+  }
+
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      <form className="search-form" action="">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Don't click me</button>
+      </form>
+
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button onClick={() => {setCounter(counter + 1)}} type="button">Click Me</button>
+      </form>
+      <h1>{counter}</h1>
+
+      
+
+    </div>
+
+  );
+}
+
+export default App;
